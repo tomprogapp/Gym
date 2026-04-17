e23ab7d26215836dc1531faf77cd1a8d594fa226
Tom <tomas002210@gmail.com>
2026-04-17 14:43:13 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='gym-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['/Gym/']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
